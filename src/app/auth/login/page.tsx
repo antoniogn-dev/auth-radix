5eca9e894e916599a8895b5fd288d9030efcc11e
antoniogn-dev <antoniogn2810@gmail.com>
2026-08-09 09:46:50 -0500

    instalando radix 9/08/2026

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,14 @@
+import { BookmarkIcon } from "@radix-ui/react-icons"
+import { Button } from "@radix-ui/themes"
+
+const LoginPage = () => {
+    return (
+        <div>
+            <Button>
+               <BookmarkIcon width={20} height={20} /> Login
+            </Button>
+
+        </div>
+    )
+}
+export default LoginPage
