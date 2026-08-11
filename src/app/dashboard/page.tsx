1fa1c851b692e61224715d62692d421deb5d29b5
antoniogn-dev <antoniogn2810@gmail.com>
2026-08-11 09:17:19 -0500

    taskform ui terminado 11/08/2026

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,19 @@
+"use client"
+
+import { Button, Container, Heading } from "@radix-ui/themes"
+import { useRouter } from "next/navigation"
+
 const DashboardPage = () => {
-  return (
-    <div>DashboardPage</div>
-  )
+    
+    const router = useRouter()
+
+    return (
+        <Container className="mt-10">
+            <div className="flex justify-between">
+                <Heading>Tasks</Heading>
+                <Button onClick={() => router.push("/dashboard/task/new")}>Add Task</Button>
+            </div>
+        </Container>
+    )
 }
 export default DashboardPage
---
+++ src/app/dashboard/task/new/page.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import { Button, Card, Container, Flex, Heading, TextArea, TextField } from "@radix-ui/themes"
+import { useForm, Controller } from "react-hook-form"
+
+const TaskNewPage = () => {
+
+    const {control, handleSubmit } = useForm({
+        defaultValues: {
+            title: "",
+            description: ""
+        }
+    })
+
+    const onSubmit = handleSubmit(data => {
+        console.log(data);
+        
+    })
+    
+    return (
+        <div>
+            <Container size="1" height="100%" className="p-3 md:p-0">
+                <Flex className="h-screen w-full items-center">
+                    <Card className="w-full p-7">
+                        <Heading mb="4">Create Project</Heading>
+                        <form onSubmit={onSubmit} className="flex flex-col gap-y-2">
+
+                            <label htmlFor="">Project Title</label>
+
+                            <Controller 
+                                name="title"
+                                control={control}
+                                render={({field}) => {
+                                    return (
+                                        <TextField.Root {...field} size="3" placeholder="Title…" />
+                                    )
+                                }}
+                            />
+                            <label htmlFor="">Description</label>
+
+                            <Controller
+                                name="description"
+                                control={control}
+                                render={({field}) => {
+                                    return (
+                                        <TextArea {...field} size="3" placeholder="Description..." />
+                                    )
+                                }}
+                            />
+
+                            <Button>
+                                Create Project
+                            </Button>
+
+                        </form>
+                    </Card>
+                </Flex>
+
+            </Container>
+
+        </div>
+    )
+}
+export default TaskNewPage
